448203b4e1ca72b34efdd53276f148c8fea66212
James Bonner <jameseqbonner@gmail.com>
2020-12-31 19:46:49 -0700

    made the constant constant

--- PythonPasswordManager/Common/Constants/__init__.py
+++ PythonPasswordManager/Common/Constants/__init__.py
@@ -1,4 +1,6 @@
+# makes them constant
+# https://stackoverflow.com/a/2682752
 from typing import Final
 
 # the message encrypted in the encryption table
-encryption_message = "275b2199-bebb-436a-8072-9d98690b2f11-5c83496c-7418-4243-86e5-6980351c311c"
+encryption_message: Final = "275b2199-bebb-436a-8072-9d98690b2f11-5c83496c-7418-4243-86e5-6980351c311c"
