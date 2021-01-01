f298ef1607dcacbf99d4bb8dc1997dcaed2513b2
James Bonner <jameseqbonner@gmail.com>
2020-12-31 22:01:42 -0700

    added one more variable

--- PythonPasswordManager/Common/Constants/__init__.py
+++ PythonPasswordManager/Common/Constants/__init__.py
@@ -1,5 +1,5 @@
-# the message encrypted in the encryption table
-ENCRYPTION_MESSAGE = "275b2199-bebb-436a-8072-9d98690b2f11-5c83496c-7418-4243-86e5-6980351c311c"
-
 DECRYPTED_DATABASE_NAME = "decrypted_password_database.db"
+
 ENCRYPTED_DATABASE_NAME = "encrypted_password_database.db"
+
+SETTINGS_FILE_NAME = "password_manager_settings.json"
